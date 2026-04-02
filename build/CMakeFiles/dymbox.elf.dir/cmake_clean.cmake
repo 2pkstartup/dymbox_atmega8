646d065146e3e27e705c86887a524ee15699f95c
Pavel Kryštof Herzog <2pkstartup@gmail.com>
2026-04-02 12:54:33 +0200

    cmake

---
+++ build/CMakeFiles/dymbox.elf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dymbox.elf.dir/main.c.obj"
+  "CMakeFiles/dymbox.elf.dir/main.c.obj.d"
+  "dymbox.elf"
+  "dymbox.elf.pdb"
+  "dymbox.hex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dymbox.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/dymbox.elf.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for dymbox.elf.
+# This may be replaced when dependencies are built.
